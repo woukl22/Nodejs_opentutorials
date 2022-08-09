4923c47543a09a86d4cc11a013ea31f89a5fb4c8
woukl22 <woukl22@gmail.com>
2022-08-10 08:14:09 +0900

    01-17-JavaScript-조건문

---
+++ 01-자바스크립트와 Node.js/syntax/conditional.js
@@ -0,0 +1,14 @@
+/* 
+    Conditional Statement(조건문)
+    - 조건문은 제어문의 대표적인 사례
+    - 조건에 따라 서로 다른 코드가 동작하게 프로그램의 실행 흐름을 제어하는 제어문이다.
+*/
+
+console.log('A');
+console.log('B');
+if(false){
+    console.log('C1');
+} else {
+    console.log('C2');
+}
+console.log('D');

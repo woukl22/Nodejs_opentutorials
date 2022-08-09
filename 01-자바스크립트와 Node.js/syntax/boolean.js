100a2a5f58bc686a5b4bd14c575f5d2ac2d1142e
woukl22 <woukl22@gmail.com>
2022-08-09 15:50:57 +0900

    01-14-JavaScript-Boolean

---
+++ 01-자바스크립트와 Node.js/syntax/boolean.js
@@ -0,0 +1,15 @@
+/*
+    Boolean은 두 개의 데이터타입으로 이루어짐.
+    - true
+    - false
+*/
+
+console.log(true);
+console.log(false);
+
+/*
+    true = 1;
+    이렇게 하면 에러가 난다.
+    true라고 하는 텍스트는 boolean의 데이터타입으로 약속했기 때문에 변수 이름으로 사용할 수 없다.
+    false도 동일하다.
+*/

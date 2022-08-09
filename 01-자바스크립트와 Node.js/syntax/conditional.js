332c3114c41ece248d350f296f9677a95f67617a
woukl22 <woukl22@gmail.com>
2022-08-10 08:32:50 +0900

    01-18-NodeJS-콘솔에서의 입력값

--- 01-자바스크립트와 Node.js/syntax/conditional.js
+++ 01-자바스크립트와 Node.js/syntax/conditional.js
@@ -3,10 +3,11 @@
     - 조건문은 제어문의 대표적인 사례
     - 조건에 따라 서로 다른 코드가 동작하게 프로그램의 실행 흐름을 제어하는 제어문이다.
 */
-
+var args = process.argv;
+console.log(args[2]);
 console.log('A');
 console.log('B');
-if(false){
+if(args[2] === '1'){
     console.log('C1');
 } else {
     console.log('C2');

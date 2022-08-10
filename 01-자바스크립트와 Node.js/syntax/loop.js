dd441ac218f278cf7df9d0cfb6ad45630ca8d431
woukl22 <woukl22@gmail.com>
2022-08-10 09:18:02 +0900

    01-20-JavaScript-반복문

---
+++ 01-자바스크립트와 Node.js/syntax/loop.js
@@ -0,0 +1,16 @@
+console.log('A');
+console.log('B');
+/*
+    특정 부분을  반복해야할 때 사용하는 제어문이 반복문이다.
+    반복문에는 여러 가지 종류가 있는데,
+    이번에는 while을 배워볼 것이다.
+*/
+
+var i = 0;
+while(i < 2){    // 조건이 true인 동안 while 안에 있는 값이 반복적으로 실행된다.
+    console.log('C1');
+    console.log('C2');
+    i = i + 1;
+}
+
+console.log('D');

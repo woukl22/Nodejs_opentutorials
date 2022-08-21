da73b874aa73ce70ee6f8c4abe0d3919555e00f2
woukl22 <woukl22@gmail.com>
2022-08-21 21:38:32 +0900

    01-32-App 제작-POST 방식으로 전송된 데이터 받기

--- 01-Node.js/main.js
+++ 01-Node.js/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 /*
   함수는 재사용할 수 있는 껍데기 정도로 이야기할 수 있다.
@@ -73,7 +74,7 @@ var app = http.createServer(function(request,response){
           var title = 'WEB - create';
           var list = templateList(filelist);
           var template = templateHTML(title, list, `
-            <form action="http://localhost:3000/process_create" method="post">
+            <form action="http://localhost:3000/create_process" method="post">
               <p><input type="text" name="title" placeholder="title"></p>
               <p>
                 <textarea name="description" placeholder="description"></textarea>
@@ -86,6 +87,37 @@ var app = http.createServer(function(request,response){
           response.writeHead(200); // 200이라는 숫자를 서버가 브라우저에게 주면, 파일이 성공적으로 전송됐다라는 의미이다.
           response.end(template);
         })
+      } else if(pathname === '/create_process'){
+        var body = '';
+        /*
+          request.on('data' function(data){});는 
+          웹 브라우저가 POST 방식으로 데이터를 전송할 때,
+          전송되는 데이터가 많을 경우를 대비해서 제공하는 방식이다.   => 데이터가 많으면 문제가 생김(ex: 프로그램이 꺼지거나, 컴퓨터에 무리가 감)
+
+          특정한 양의 데이터 중에서 조각조각의 데이터를 서버쪽에서 수신할 때마다 callback함수를 호출하도록 약속되어 있다.
+          호출할 때 data라는 인자를 통해 수신한 정보를 주기로 약속되어 있다.
+
+          body 데이터에 callback함수가 호출될 때마다 data를 추가해준다.
+
+          그렇게 데이터가 조각조각 들어오다가 더 이상 들어올 정보가 없으면 
+          request.on('end', function(){});에서 'end' 뒤에 나오는 callback함수를 호출하기로 약속되어 있다.
+          따라서 end 뒤에 나오는 callback함수가 실행됐을 때 정보 수신이 끝났다라고 생각할 수 있다.
+
+          아래 data와 end를 이벤트라고 하는데, 
+          이 이벤트를 이용해서 POST 방식으로 전송된 데이터를 가져올 수 있고,
+          또 querystring이라는 모듈의 parse 함수를 이용해서 정보를 객체화할 수있다.
+        */
+        request.on('data', function(data){
+          body = body + data;
+        });
+        request.on('end', function(){
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description;
+          console.log(post.title);
+        });
+        response.writeHead(200);
+        response.end('success');
       } else{
         response.writeHead(200);  // 파일을 찾을 수 없는 경우, 웹 서버는 404라는 번호를 돌려준다.
         response.end('Not found');

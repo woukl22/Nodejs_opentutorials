221e7bfb9e423e39043b209aff5384f1ecd9465d
woukl22 <woukl22@gmail.com>
2022-08-23 12:07:37 +0900

    01-39-JavaScript 객체의 형식

---
+++ 01-Node.js/syntax/object.js
@@ -0,0 +1,16 @@
+var members = ['egoing', 'k8805', 'hoya'];
+console.log(members[1]);    // 출력: k8805
+/*
+    두 번째 정보를 가져오고 싶다면 [1]을 써줘야 한다.
+    배열은 식별자가 숫자로 되어 있음.
+*/
+
+var roles = {
+    'programmer' : 'egoing',
+    'designer' : 'k8805',
+    'manager' : 'hoya'
+}
+console.log(roles.designer);    // 출력: k8805
+/*
+    객체는 각각의 데이터마다 고유한 이름을 줄 수 있다.
+*/
